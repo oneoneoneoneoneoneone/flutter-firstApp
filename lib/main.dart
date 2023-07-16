8ba89b0996170cddc75266268cacacc4ce7d6544
oneoneoneoneoneoneone <0ne1_1@naver.com>
2023-07-17 01:16:10 +0900

    [Feat] 탐색 네이게이션 반응형 처리

--- lib/main.dart
+++ lib/main.dart
@@ -83,44 +83,51 @@ class _MyHomePageState extends State<MyHomePage> {
         throw UnimplementedError('no widget for $selectedIndex');
     }
 
-    // Scaffold - 최상위 위젯. build() 메서드가 반환할 위젯 or 위젯 트리
-    return Scaffold(
-      body: Row(
-        children: [
-          // SafeArea - 하위 요소가 하드웨어 노치나 상태 표시줄로 가려지지 않음
-          SafeArea(
-            child: NavigationRail(
-              // extended: false - 아이콘 옆에 라벨 표시하지 않음
-              extended: false,
-              destinations: [
-                NavigationRailDestination(
-                  icon: Icon(Icons.home),
-                  label: Text('Home'),
+    // 사용할 수 있는 공간의 양에 따라 위젯 트리를 변경
+    return LayoutBuilder(
+      // builder - 제약 조건(창 크기 변경, 가로/세로 모드 변경, 타 위젯 크기 변경)이 변경될 때마다 호출됨
+      // constraints - MyHomePage의 제약조건이 정의되어 있음
+      builder: (context, constraints) {
+        // Scaffold - 최상위 위젯. build() 메서드가 반환할 위젯 or 위젯 트리
+        return Scaffold(
+          body: Row(
+            children: [
+              // SafeArea - 하위 요소가 하드웨어 노치나 상태 표시줄로 가려지지 않음
+              SafeArea(
+                child: NavigationRail(
+                  // extended: false - 아이콘 옆에 라벨 표시하지 않음
+                  extended: constraints.maxWidth >= 600,
+                  destinations: [
+                    NavigationRailDestination(
+                      icon: Icon(Icons.home),
+                      label: Text('Home'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.favorite),
+                      label: Text('Favorites'),
+                    ),
+                  ],
+                  selectedIndex: selectedIndex,
+                  // onDestinationSelected - NavigationRailDestination을 선택했을 때 동작 정의
+                  onDestinationSelected: (value) {
+                    setState(() {
+                      selectedIndex = value;
+                    });
+                  },
                 ),
-                NavigationRailDestination(
-                  icon: Icon(Icons.favorite),
-                  label: Text('Favorites'),
+              ),
+              // Expanded - 같은 계층의 위젯(NavigationRail)이 필요로 하는 공간 외에 남은 공간을 최대한 차지하는 레이아웃 표현
+              Expanded(
+                // Container - ?
+                child: Container(
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                  child: page,
                 ),
-              ],
-              selectedIndex: selectedIndex,
-              // onDestinationSelected - NavigationRailDestination을 선택했을 때 동작 정의
-              onDestinationSelected: (value) {
-                setState(() {
-                  selectedIndex = value;
-                });
-              },
-            ),
+              ),
+            ],
           ),
-          // Expanded - 같은 계층의 위젯(NavigationRail)이 필요로 하는 공간 외에 남은 공간을 최대한 차지하는 레이아웃 표현
-          Expanded(
-            // Container - ?
-            child: Container(
-              color: Theme.of(context).colorScheme.primaryContainer,
-              child: page,
-            ),
-          ),
-        ],
-      ),
+        );
+      }
     );
   }
 }
